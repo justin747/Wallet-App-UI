c7bcca5c188f8f34968c723491e79e71db10ee4b
justin747 <justin747@gmail.com>
2022-08-23 18:24:02 -0700

    Initial Commit

---
+++ Wallet App UI/Wallet_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet_App_UIApp.swift
+//  Wallet App UI
+//
+//  Created by Justin on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Wallet_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
